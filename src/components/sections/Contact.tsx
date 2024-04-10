57491e873a1b90265c70b48dace132df895bf7b0
SushanShakya <sushaanshakya88@gmail.com>
2024-04-10 15:57:51 +0545

    Feat : Github config

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -11,16 +11,10 @@ const INITIAL_STATE = Object.fromEntries(
   Object.keys(config.contact.form).map((input) => [input, ""])
 );
 
-const emailjsConfig = {
-  serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  templateId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  accessToken: import.meta.env.VITE_EMAILJS_ACCESS_TOKEN,
-};
-
 const Contact = () => {
   const formRef = useRef<React.LegacyRef<HTMLFormElement> | undefined>();
   const [form, setForm] = useState(INITIAL_STATE);
-  const [loading, setLoading] = useState(false);
+  const [loading] = useState(false);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | undefined
